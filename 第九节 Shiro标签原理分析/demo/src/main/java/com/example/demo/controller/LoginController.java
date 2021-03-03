450d17f868f4fa3d8b2871683a676a1c9fcd523e
unknown <liuguofeng-java@qq.com>
2021-03-03 13:48:55 +0800

    初始化

---
+++ 第九节 Shiro标签原理分析/demo/src/main/java/com/example/demo/controller/LoginController.java
@@ -0,0 +1,24 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class LoginController {
+
+    @GetMapping(value = {"/","login"})
+    public ModelAndView login(){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("login");
+        return modelAndView;
+    }
+
+    @ResponseBody
+    @PostMapping(value = {"submit"})
+    public String submit(){
+        return "";
+    }
+}
